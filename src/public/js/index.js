33505b3faea146cde34df3837827fb769598b0f3
Matthew Keasling <mkeasling@gmail.com>
2017-03-19 17:57:34 -0400

    adding in firebase initialization

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,12 @@
+/* eslint-env browser, commonjs */
+/* global firebase */
+
 'use strict';
+
+import { firebase as fbConfig } from '../../../config.json';
 
 require('../sass/main.scss');
 
+firebase.initializeApp(fbConfig);
 
 
